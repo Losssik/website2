59c95cea202be9a6fa56a19e29e8841b6d4fbf95
Losssik <przemek.bialkwno@gmail.com>
2022-09-07 20:21:49 +0200

    working on  tabbed component functionality

---
+++ app.js
@@ -0,0 +1,25 @@
+//tabbed component
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  console.log(clicked);
+
+  //guard clause
+  if (!clicked) return;
+  //active tab
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+  //remove active
+  tabsContent.forEach((content) =>
+    content.classList.remove("operations__content--active")
+  );
+
+  //activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
